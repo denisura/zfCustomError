608ab39a7e87d929f9ab7c9062521015ff33aa16
Denis Uraganov <beanstalk@uraganov.net>
2012-08-20 02:08:19 -0700

    Set bootsrap for front controller
    Change-Id: Id46786cc20a982a061b16b657e578d8631f39092

--- tests/application/ControllerTestCase.php
+++ tests/application/ControllerTestCase.php
@@ -1,21 +1,29 @@
 <?php
-include_once ("Zend/Test/PHPUnit/ControllerTestCase.php");
+include_once("Zend/Test/PHPUnit/ControllerTestCase.php");
 
-class ControllerTestCase extends Zend_Test_PHPUnit_ControllerTestCase
-{
+class ControllerTestCase extends Zend_Test_PHPUnit_ControllerTestCase {
 
-    protected $application;
+	protected $application;
 
-    public function setUp ()
-    {
-        $this->bootstrap = array($this, 'appBootstrap');
-        parent::setUp();
-    }
+	public function setUp() {
 
-    public function appBootstrap ()
-    {
-        $this->application = new Zend_Application(APPLICATION_ENV, 
-                APPLICATION_PATH . '/configs/application.ini');
-        return $this->application->bootstrap();
-    }
+		$this->bootstrap = array($this, 'appBootstrap');
+		parent::setUp();
+	}
+
+	public function appBootstrap() {
+		$this->application = new Zend_Application(APPLICATION_ENV,
+				APPLICATION_PATH . '/configs/application.ini');
+		return $this->application->bootstrap();
+	}
+
+	/**
+	 * Set bootstrap for fronController
+	 * @see http://stackoverflow.com/questions/1226758
+	 */
+	public function dispatch($url = null) {
+		$test = $this->application->getBootstrap();
+		$this->getFrontController()->setParam('bootstrap', $test);
+		parent::dispatch($url);
+	}
 }
